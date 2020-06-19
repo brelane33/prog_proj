28a039e86e96beb4e3e7ac13d25d4939588a13e2
rayaivy <raya0ivy@gmail.com>
2020-06-19 14:54:57 -0500

    Update bubble_r.c
    remove local implementation of array printing and array element swapping to use ppsh.h

--- c_algorithms/sorting/bubble_r.c
+++ c_algorithms/sorting/bubble_r.c
@@ -1,17 +1,12 @@
-#include <stdio.h>
-#include <stdbool.h>
+#include "PPSH.h"
 
 int main()
 {
    int nums[] = {25, 15, 19, 48, 27, 42, 40, 13, 4, 30, 39, 33, 44, 16, 49, 14, 28, 17, 47, 31};
 
   int s1 = sizeof(nums)/sizeof(nums[0]);
-  printf("Numer of elements in the array: %d\n",s1);
-  printf("Original Array:\t");
-  for(int i=0; i<s1;i++){
-    printf("%d ",nums[i]);
-  }
 
+  printarray(nums, s1);
   printf("\n");
   bool needed = 0;
   int passes = 0;
@@ -22,18 +17,14 @@ int main()
       if (nums[i] > nums[i+1])
         {
           needed=1;
-          int tmp=nums[i];
-          nums[i] = nums[i+1];
-          nums[i+1] = tmp;
+          swapints(&nums[i],&nums[i+1]);
         }
       }
       passes++;
     } while(needed);
 
   printf("Sorted Array:\t");
-  for(int i=0; i<s1;i++){
-    printf("%d ",nums[i]);
-  }
+  printarray(nums, s1);
   printf("\n");
   printf("Number of passes: %d\n", passes);
 
